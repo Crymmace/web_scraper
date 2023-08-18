6c522ccbac70057f4a69f4310f020d7ceaf841ab
Crystal Mace <crymace@outlook.com>
2023-08-18 21:00:30 +0200

    Rewrite with OOP

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from send_email import send_email
 import sqlite3
 import time
 
-connection = sqlite3.connect("data.db")
 
 URL = "http://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
@@ -12,54 +11,57 @@ HEADERS = {
                   '(KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 
-def scrape(url):
-    response = requests.get(url, headers=HEADERS)
-    source = response.text
-    return source
+class Event:
+    def scrape(self, url):
+        response = requests.get(url, headers=HEADERS)
+        source = response.text
+        return source
+
+    def extract(self, source):
+        extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+        value = extractor.extract(source)["tours"]
+        return value
 
 
-def extract(source):
-    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
-    value = extractor.extract(source)["tours"]
-    return value
+class Database:
+    def __init__(self, database_path):
+        self.connection = sqlite3.connect(database_path)
+
+    def store(self, extracted):
+        row = extracted.split(",")
+        row = [item.strip() for item in row]
+        cursor = self.connection.cursor()
+        cursor.execute("INSERT INTO events VALUES(?, ?, ?)", row)
+        self.connection.commit()
+
+    def read(self, extracted):
+        row = extracted.split(",")
+        row = [item.strip() for item in row]
+        band, city, date = row
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?",
+                       (band, city, date))
+        rows = cursor.fetchall()
+        return rows
 
 
-def store(extracted):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO events VALUES(?, ?, ?)", row)
-    connection.commit()
+body = """
+Subject: New event found!
 
-
-def read(extracted):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
-    band, city, date = row
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?",
-                   (band, city, date))
-    rows = cursor.fetchall()
-    return rows
-
-
-body = "Subject: New event found!" \
-       "" \
-       "There's a new event!"
+Body: There's a new event!"""
 
 if __name__ == "__main__":
     while True:
-        scraped = scrape(URL)
-        extracted = extract(scraped)
+        event = Event()
+        scraped = event.scrape(URL)
+        extracted = event.extract(scraped)
         print(extracted)
 
         if extracted != "No upcoming tours":
-            row = read(extracted)
+            database = Database("data.db")
+            row = database.read(extracted)
             if not row:
-                store(extracted)
+                database.store(extracted)
                 send_email(body)
         time.sleep(2)
-
-
-
 
